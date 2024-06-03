42a01aea9317d593fc275b0df1f882da407f38bd
Denis Moreira <silvad281@gmail.com>
2024-06-02 21:54:47 -0300

    feat: criando função para consumir api do Cosmic cms

---
+++ src/utils/actions/get-data.ts
@@ -0,0 +1,15 @@
+export async function getDataHome() {
+   try {
+      const res = await fetch(
+         `${process.env.NEXT_PUBLIC_API_API}/objects/665d0305b6cce150ff098de3?read_key=${process.env.READ_KEY}&props=slug,title,metadata`
+      );
+
+      if (!res.ok) {
+         throw new Error("Failed to fetch data!");
+      }
+
+      return res.json();
+   } catch (error: any) {
+      throw new Error(error);
+   }
+}
