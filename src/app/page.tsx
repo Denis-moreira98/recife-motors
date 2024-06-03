631ade6b1ea648903f83a7d1239f5e5fe1e6a849
Denis Moreira <silvad281@gmail.com>
2024-06-03 11:36:21 -0300

    feat: montando page Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,32 @@
-import { Submenu } from "./home/submenu";
+import { Submenu } from "@/components/home/submenu";
+import { Hero } from "@/components/hero";
+import { HomeProps } from "@/utils/home.type";
+import { getDataHome } from "@/utils/actions/get-data";
+import { FaWhatsapp } from "react-icons/fa";
+import { Services } from "@/components/home/services";
+import { Container } from "@/components/container";
+import { Footer } from "@/components/home/footer";
 
-export default function Home() {
+export default async function Home() {
+   const { object }: HomeProps = await getDataHome();
+
+   console.log();
+
    return (
       <main>
          <Submenu />
+         <Hero
+            heading={object.metadata.heading}
+            buttonTitle={object.metadata.cta_button.title}
+            buttonUrl={object.metadata.cta_button.url}
+            banner={object.metadata.banner.url}
+            icon={<FaWhatsapp size={24} color="#fff" />}
+         />
+
+         <Container>
+            <Services object={object} />
+            <Footer object={object} />
+         </Container>
       </main>
    );
 }
