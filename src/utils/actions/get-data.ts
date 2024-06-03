3160db6878fe44b0a5d4ed2df512ff6fc18b5c30
Denis Moreira <silvad281@gmail.com>
2024-06-03 14:18:48 -0300

    feat: add função para buscar dados do submenu

--- src/utils/actions/get-data.ts
+++ src/utils/actions/get-data.ts
@@ -1,7 +1,8 @@
 export async function getDataHome() {
    try {
       const res = await fetch(
-         `${process.env.NEXT_PUBLIC_API}/objects/665d0305b6cce150ff098de3?read_key=${process.env.READ_KEY}&props=slug,title,metadata`
+         `${process.env.NEXT_PUBLIC_API}/objects/665d0305b6cce150ff098de3?read_key=${process.env.READ_KEY}&props=slug,title,metadata`,
+         { next: { revalidate: 120 } }
       );
 
       if (!res.ok) {
@@ -11,5 +12,22 @@ export async function getDataHome() {
       return res.json();
    } catch (error: any) {
       throw new Error(error);
+   }
+}
+
+export async function getDataSubMenu() {
+   try {
+      const res = await fetch(
+         `${process.env.NEXT_PUBLIC_API}/objects?pretty=true&query=%7B%22type%22:%22pages%22%7D&limit=10&read_key=${process.env.READ_KEY}&props=slug,title,`,
+         { next: { revalidate: 120 } }
+      );
+
+      if (!res.ok) {
+         throw new Error("Failed to fetch data!");
+      }
+
+      return res.json();
+   } catch (error: any) {
+      throw new Error("Failed to get data", error);
    }
 }
