7b328df90c100c0da98619705c88117c8c959836
Denis Moreira <silvad281@gmail.com>
2024-06-04 14:23:34 -0300

    feat: config metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,21 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
    title: "Recife Motors - Sua oficina especializada!",
    description: "Oficina especializada em carros de Recife-PE!",
+   keywords: ["oficina", "oficina carros", "carros", "manutenção de carros"],
+   openGraph: {
+      title: "DevMotors - Sua oficina especializada!",
+      images: [`${process.env.NEXT_PUBLIC_URL}/project.png`],
+   },
+   robots: {
+      index: true,
+      follow: true,
+      nocache: true,
+      googleBot: {
+         index: true,
+         follow: true,
+         noimageindex: true,
+      },
+   },
 };
 
 export default function RootLayout({
