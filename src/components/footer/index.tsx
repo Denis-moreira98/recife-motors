e728d31b4a31806e7a02d7fca62903bdf5f7d67f
Denis Moreira <silvad281@gmail.com>
2024-06-04 15:50:24 -0300

    style: ajuste no submenu e no footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 export function Footer() {
    return (
       <p className={styles.copyText}>
-         Todos direitos reservados <strong>Recife Motors</strong> Copyright
+         Todos direitos reservados <strong>Recife Motors</strong>
          &copy; {""}
          {`${new Date().getFullYear()}`}
       </p>
